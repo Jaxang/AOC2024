2472c195458fdc15983ea1405a1a98e67b8108c2
Johan Jaxing <johan.jaxing@gmail.com>
2024-12-11 21:47:21 +0100

    Day11 star2

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fs::File;
+use std::hash::Hash;
 use std::io::{self, BufRead};
 use std::path::Path;
 
@@ -11,66 +12,53 @@ pub fn run(filename: &str) -> io::Result<()> {
     Ok(())
 }
 
-fn star1(line: &[i64]) -> i32 {
-    println!("{:?}", line);
-    let mut sum = 0;
-    let mut visited = HashMap::new();
-    for num in line.iter() {
-        sum += calculate_stones(*num, 25, &mut visited);
-    }
-    sum as i32
+fn star1(line: &[i64]) -> i64 {
+    let sum = calculate_stones_bfs(line, 25);
+    sum
 }
 
-fn star2(line: &[i64]) -> i32 {
-    let mut sum = 0;
-    let mut visited = HashMap::new();
-    for num in line.iter() {
-        sum += calculate_stones(*num, 75, &mut visited);
-    }
-    sum as i32
+fn star2(line: &[i64]) -> i64 {
+    let sum = calculate_stones_bfs(line, 75);
+    sum
 }
-
-fn calculate_stones(num: i64, depth: usize, visited: &mut HashMap<i64, (i64, i64, usize)>) -> u32 {
-    if depth == 0 {
-        return 1;
+fn calculate_stones_bfs(nums: &[i64], depth: usize) -> i64 {
+    let mut nums_counter = HashMap::new();
+    for num in nums {
+        add_count(&mut nums_counter, *num, 1);
     }
 
-    if let Some((new_num_l, new_num_r, depth_diff)) = visited.clone().get(&num) {
-        //println!("hej");
-        if depth_diff > &depth {
-            return 1;
-        }
-        let left_n = calculate_stones(*new_num_l, depth - depth_diff, visited);
-        let right_n = calculate_stones(*new_num_r, depth - depth_diff, visited);
-        return left_n + right_n;
-    }
-
-    let mut traversed = Vec::new();
-    let mut new_num = num;
-    let mut new_depth = depth;
-    if num == 0 {
-        traversed.push((0, new_depth));
-        new_num = 1;
-        new_depth -= 1;
-    }
-    while new_depth > 0 {
-        match split_if_even_length(new_num) {
-            Some((left, right)) => {
-                new_depth -= 1;
-                for (n, d) in traversed.iter() {
-                    visited.insert(*n, (left, right, d - new_depth));
+    let mut d = depth;
+    while d > 0 {
+        let mut new_nums_counter = HashMap::new();
+        for (num, &count) in nums_counter.iter() {
+            if *num == 0 {
+                add_count(&mut new_nums_counter, 1, count);
+            } else {
+                match split_if_even_length(*num) {
+                    Some((left, right)) => {
+                        add_count(&mut new_nums_counter, left, count);
+                        add_count(&mut new_nums_counter, right, count);
+                    }
+                    None => {
+                        add_count(&mut new_nums_counter, num * 2024, count);
+                    }
                 }
-                let left_n = calculate_stones(left, new_depth, visited);
-                let right_n = calculate_stones(right, new_depth, visited);
-                return left_n + right_n;
-            }
-            None => {
-                new_num *= 2024;
-                new_depth -= 1;
             }
         }
+        nums_counter = new_nums_counter;
+        d -= 1;
     }
-    1
+
+    let mut sum = 0;
+    for (_, &count) in nums_counter.iter() {
+        sum += count;
+    }
+    sum
+}
+
+fn add_count(counter: &mut HashMap<i64, i64>, num: i64, count: i64) {
+    let old_count = counter.get(&num).unwrap_or(&0);
+    counter.insert(num, old_count + count);
 }
 
 fn split_if_even_length(num: i64) -> Option<(i64, i64)> {
