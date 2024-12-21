27922b88d63fa856976757a865d6bdb3160d2208
Johan Jaxing <johan.jaxing@gmail.com>
2024-12-21 15:44:52 +0100

    day29 solved

--- src/day20.rs
+++ src/day20.rs
@@ -1,6 +1,6 @@
 use priority_queue::PriorityQueue;
 use std::cmp::Reverse;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -15,58 +15,21 @@ pub fn run(filename: &str) -> io::Result<()> {
 
 fn star1(grid: &[Vec<char>]) -> i32 {
     let (start, end) = get_start_and_end(grid);
-    let mut start_dist = vec![vec![-1; grid[0].len()]; grid.len()];
     let mut end_dist = vec![vec![-1; grid[0].len()]; grid.len()];
-    shortest_path(end, &grid, &mut end_dist);
-    shortest_path(start, &grid, &mut start_dist);
-    let start_target = start_dist[end.0 as usize][end.1 as usize];
-    let end_target = end_dist[start.0 as usize][start.1 as usize];
-    println!("{}, {}", start_target, end_target);
-    let mut best_cheats = cheated_paths(
-        end_target as i32 - 49,
-        1,
-        start,
-        end,
-        grid,
-        &start_dist,
-        &end_dist,
-    );
-    best_cheats.sort();
-    println!("{:?}", best_cheats);
+    shortest_path(end, grid, &mut end_dist);
+    let best_cheats = cheated_path(100, 2, start, grid, &end_dist);
     best_cheats.len() as i32
 }
 
 fn star2(grid: &[Vec<char>]) -> i32 {
     let (start, end) = get_start_and_end(grid);
-    let mut start_dist = vec![vec![-1; grid[0].len()]; grid.len()];
     let mut end_dist = vec![vec![-1; grid[0].len()]; grid.len()];
-    shortest_path(end, &grid, &mut end_dist);
-    shortest_path(start, &grid, &mut start_dist);
-    let start_target = start_dist[end.0 as usize][end.1 as usize];
-    let end_target = end_dist[start.0 as usize][start.1 as usize];
-    println!("{}, {}", start_target, end_target);
-    let best_cheats = cheated_paths(
-        end_target as i32 - 49,
-        19,
-        start,
-        end,
-        grid,
-        &start_dist,
-        &end_dist,
-    );
-    let mut occurrences = HashMap::new();
-    for cheat in &best_cheats {
-        *occurrences.entry(cheat).or_insert(0) += 1;
-    }
-    let mut sorted_occurrences: Vec<_> = occurrences.iter().collect();
-    sorted_occurrences.sort_by(|a, b| Reverse(b.0).cmp(&Reverse(a.0)));
-    for (cheat, count) in sorted_occurrences {
-        println!("{:?}: {}", cheat, count);
-    }
+    shortest_path(end, grid, &mut end_dist);
+    let best_cheats = cheated_path(100, 20, start, grid, &end_dist);
     best_cheats.len() as i32
 }
 
-fn shortest_path(end_pos: (i32, i32), grid: &[Vec<char>], dist: &mut Vec<Vec<i32>>) {
+fn shortest_path(end_pos: (i32, i32), grid: &[Vec<char>], dist: &mut [Vec<i32>]) {
     let mut pq = PriorityQueue::new();
 
     pq.push((end_pos, 0), Reverse(0));
@@ -92,64 +55,75 @@ fn shortest_path(end_pos: (i32, i32), grid: &[Vec<char>], dist: &mut Vec<Vec<i32
     }
 }
 
-fn cheated_paths(
+fn cheated_path(
     target: i32,
     cheat_length: i32,
     start_pos: (i32, i32),
-    end_pos: (i32, i32),
     grid: &[Vec<char>],
-    start_dist: &Vec<Vec<i32>>,
     end_dist: &Vec<Vec<i32>>,
 ) -> Vec<i32> {
     let mut pq = PriorityQueue::new();
     let mut visited = HashSet::new();
     let mut outputs = Vec::new();
-    let cheated_positions = ((0, 0), 0, (-1, -1));
-    pq.push((start_pos, 0, cheated_positions), Reverse(0));
+    pq.push((start_pos, 0), Reverse(0));
     while !pq.is_empty() {
-        let ((pos, steps, cheated), _) = pq.pop().unwrap();
+        let ((pos, steps), _) = pq.pop().unwrap();
         let (i, j) = pos;
-        let mut new_cheated = cheated.clone();
 
-        if steps > target {
-            break;
+        if visited.contains(&pos) {
+            continue;
         }
+        visited.insert(pos);
         if i < 0 || i >= grid.len() as i32 || j < 0 || j >= grid[0].len() as i32 {
             continue;
         }
         if grid[i as usize][j as usize] == '#' {
-            if cheated.1 < cheat_length {
-                new_cheated = (cheated.0, cheated.1 + 1, (-1, -1));
-            } else {
-                continue;
-            }
-        }
-        if new_cheated.1 == 0 {
-            new_cheated = (pos, 0, (-1, -1));
-        } else if grid[i as usize][j as usize] != '#' && cheated.2 == (-1, -1) {
-            new_cheated = (new_cheated.0, new_cheated.1 + 1, pos);
-            let original_steps = start_dist[pos.0 as usize][pos.1 as usize];
-            let original_steps_left = end_dist[pos.0 as usize][pos.1 as usize];
-            let new_steps = original_steps_left + steps;
-            let saved = original_steps_left + original_steps - new_steps;
-            if !visited.contains(&(pos, (new_cheated.0, new_cheated.2))) && new_steps < target {
-                outputs.push(saved);
-            }
-            if cheated.1 == cheat_length {
-                visited.insert((pos, (new_cheated.0, new_cheated.2)));
-            }
-        }
-        if visited.contains(&(pos, (new_cheated.0, new_cheated.2))) {
             continue;
         }
+        cheat(target, cheat_length, pos, grid, end_dist, &mut outputs);
 
-        visited.insert((pos, (new_cheated.0, new_cheated.2)));
-        pq.push(((i + 1, j), steps + 1, new_cheated), Reverse(steps + 1));
-        pq.push(((i - 1, j), steps + 1, new_cheated), Reverse(steps + 1));
-        pq.push(((i, j + 1), steps + 1, new_cheated), Reverse(steps + 1));
-        pq.push(((i, j - 1), steps + 1, new_cheated), Reverse(steps + 1));
+        pq.push(((i + 1, j), steps + 1), Reverse(steps + 1));
+        pq.push(((i - 1, j), steps + 1), Reverse(steps + 1));
+        pq.push(((i, j + 1), steps + 1), Reverse(steps + 1));
+        pq.push(((i, j - 1), steps + 1), Reverse(steps + 1));
     }
     outputs
+}
+
+fn cheat(
+    target: i32,
+    cheat_length: i32,
+    start_pos: (i32, i32),
+    grid: &[Vec<char>],
+    end_dist: &Vec<Vec<i32>>,
+    outputs: &mut Vec<i32>,
+) {
+    let (oi, oj) = start_pos;
+    for i in -cheat_length..cheat_length + 1 {
+        let ni = oi + i;
+        let i_diff = i.abs();
+        let j_max = cheat_length - i_diff;
+        for j in -j_max..j_max + 1 {
+            let nj = oj + j;
+            let j_diff = j.abs();
+            let ham_dist = i_diff + j_diff;
+            if ni < 0
+                || ni >= grid.len() as i32
+                || nj < 0
+                || nj >= grid[0].len() as i32
+                || grid[ni as usize][nj as usize] == '#'
+            {
+                continue;
+            }
+            //let original_steps = start_dist[pos.0 as usize][pos.1 as usize];
+            let original_steps_left = end_dist[oi as usize][oj as usize];
+            let steps_left = end_dist[ni as usize][nj as usize];
+            let saved = original_steps_left - steps_left - ham_dist;
+            if saved >= target {
+                outputs.push(saved);
+            }
+        }
+    }
 }
 
 fn get_start_and_end(gird: &[Vec<char>]) -> ((i32, i32), (i32, i32)) {
